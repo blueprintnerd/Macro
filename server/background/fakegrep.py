e78ccf09d786e531f866e19475c950e33f037747
Sterling Duncan <support@scutoid.top>
2026-06-20 06:38:16 +0000

    ive deleted everything. the current code i had was impossible to read

--- server/background/fakegrep.py
+++
@@ -1,7 +0,0 @@
-import os
-import sqlite3
-
-def grep_contents_for_drive(text):
-    connection = sqlite3.connect("music.")
-    #!() as a way to signify the character has been replaced
-    sanitized_input = text.replace("\\", "!(/)" )
