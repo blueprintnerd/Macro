4891ce6ad52020541a5e7decb18c45082032cd77
Sterling Duncan <support@scutoid.top>
2026-06-23 09:47:03 +0000

    commit

--- server/mmanager.py
+++ server/mmanager.py
@@ -139,7 +139,7 @@ class SemanticSearch(Screen):
         yield Static("Select a search backend")
         with RadioSet(id="search_backend"):
             yield RadioButton("Use Grep as a backend")
-            yield SemanticButton
+            yield SemanticButton()
 
 if __name__ == "__main__":
     app = MacroManager()
