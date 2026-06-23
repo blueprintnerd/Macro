3679db8f656e785a85f0fe15e80ef60e5e269003
Sterling Duncan <support@scutoid.top>
2026-06-23 09:51:38 +0000

    commit

--- server/mmanager.py
+++ server/mmanager.py
@@ -143,7 +143,7 @@ class SemanticSearch(Screen):
             yield RadioButton("Use a embedding model (Requires at least 4GB of ram)")
             yield RadioButton("Manually set uo keywords show results on search")
             yield RadioButton("Do not use search")
-        Button("")
+        Button("Continue")
 
 if __name__ == "__main__":
     app = MacroManager()
