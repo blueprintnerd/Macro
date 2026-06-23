84df3b20e43b80f06ec8bfad69f662e36981323d
Sterling Duncan <support@scutoid.top>
2026-06-23 09:49:21 +0000

    commit

--- server/mmanager.py
+++ server/mmanager.py
@@ -141,7 +141,8 @@ class SemanticSearch(Screen):
             yield RadioButton("Use Grep as a backend")
             yield RadioButton("Use a embedding model (Requires at least 4GB of ram)")
             yield RadioButton("Manually set uo keywords show results on search")
-            yield Radiozbutton("")
+            yield RadioButton(
+            )
 
 if __name__ == "__main__":
     app = MacroManager()
